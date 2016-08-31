9675909153ac1ad387714c4c3279c9e4e6eaf22c
yongsang <yongsang@publy.co>
2016-08-31 15:01:11 +0900

    add FacebookService

---
+++ src/FacebookService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Publy\ServiceClient;
+
+use Publy\ServiceClient\Api\BaseApiService;
+
+class FacebookService extends BaseApiService {
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->domain = 'https://graph.facebook.com';
+        $this->apiUrl = "$this->domain/";
+    }
+
+    public function getUserInfo($accessToken, $fields) {
+    	return $this->get('me', array('fields' => $fields,
+                                      'access_token' => $accessToken));
+    }
+}
