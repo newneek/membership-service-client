d70f8a26437e9a31e86fa493821d54f41ac9c539
yongsang <yongsang@publy.co>
2017-06-01 14:00:37 +0900

    add cow path

--- copy_dev.sh
+++ copy_dev.sh
@@ -4,6 +4,7 @@ admin_l5=$BASEDIR"/../admin-l5";
 batch_service=$BASEDIR"/../batch-service";
 content_service=$BASEDIR"/../content-service";
 payment_service=$BASEDIR"/../payment-service";
+cow=$BASEDIR"/../cow";
 www_l5=$BASEDIR"/../www-l5";
 
 # Define your function here
@@ -21,6 +22,7 @@ copy_src_to_vendor $admin_l5
 copy_src_to_vendor $batch_service
 copy_src_to_vendor $content_service
 copy_src_to_vendor $payment_service
+copy_src_to_vendor $cow
 copy_src_to_vendor $www_l5
 
 echo 'finish'
