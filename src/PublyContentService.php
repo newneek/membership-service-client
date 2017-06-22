8f49abb5fbea01739bb7e8758e2e98b4525f97f3
yongsang <yongsang@publy.co>
2017-06-22 11:14:48 +0900

    getTotalPackageReader

--- src/PublyContentService.php
+++ src/PublyContentService.php
@@ -1115,4 +1115,9 @@ class PublyContentService extends BaseApiService
     {
         return $this->post("package_reader/delete", $params);
     }
+
+    public function getTotalPackageReader($userId)
+    {
+        return $this->get("package_reader/total", ['user_id' => $userId]);
+    }
 }
