f62b885ce6d667fd436821d648853b4c6af723c3
Seungkook Lee <sk@publy.co>
2018-10-13 17:03:39 +0900

    add recombee service

--- src/RecombeeService.php
+++ src/RecombeeService.php
@@ -2,27 +2,100 @@
 
 namespace Publy\ServiceClient;
 
-use Recombee\RecommApi\Client;
-use Recombee\RecommApi\Requests as Reqs;
-use Recombee\RecommApi\Exceptions as Ex;
-
+use Recombee\RecommApi\Client as RecombeeClient;
+use Recombee\RecommApi\Requests as RecombeeRequests;
 
 class RecombeeService
 {
     protected $client;
+    const SET_ITEM_PREFIX = 'set:';
 
     public function __construct($databaseId, $secretToken)
     {
-        $this->client = new Client($databaseId, $secretToken);
+        $this->client = new RecombeeClient($databaseId, $secretToken);
     }
 
-    public function viewContent($userId, $contentId)
+    public function viewContent($userId, $setId, $contentId)
     {
-
+        // contentId in not used currently
+        try
+        {
+            $request =
+                new RecombeeRequests\AddDetailView(
+                    $userId,
+                    static::SET_ITEM_PREFIX . $setId,
+                    [ 'cascadeCreate' => true ]
+                );
+            $result = $this->client->send($request);
+        } catch(\Exception $e) {
+            report_async_error($e);
+        }
     }
 
-    public function completeContent($userId, $contentId)
-    {// we use purchase for completion
+    public function addBookmark($userId, $setId)
+    {
+        try
+        {
+            $request =
+                new RecombeeRequests\AddBookmark(
+                    $userId,
+                    static::SET_ITEM_PREFIX . $setId,
+                    [ 'cascadeCreate' => true ]
+                );
+            $result = $this->client->send($request);
+        } catch(\Exception $e) {
+            report_async_error($e);
+        }
+    }
 
+    public function deleteBookmark($userId, $setId)
+    {
+        try
+        {
+            $request =
+                new RecombeeRequests\DeleteBookmark(
+                    $userId,
+                    static::SET_ITEM_PREFIX . $setId,
+                    [ 'cascadeCreate' => true ]
+                );
+            $result = $this->client->send($request);
+        } catch(\Exception $e) {
+            report_async_error($e);
+        }
+    }
+
+    public function AddRating($userId, $setId, $publyRating)
+    {
+        $recombeeRating = $publyRating - 2;
+        try
+        {
+            $request =
+                new RecombeeRequests\AddRating(
+                    $userId,
+                    static::SET_ITEM_PREFIX . $setId,
+                    $recombeeRating,
+                    [ 'cascadeCreate' => true ]
+                );
+            $result = $this->client->send($request);
+        } catch(\Exception $e) {
+            report_async_error($e);
+        }
+    }
+
+    public function SetViewPortion($userId, $setId, $completedContents, $totalContents)
+    {
+        try
+        {
+            $request =
+                new RecombeeRequests\SetViewPortion(
+                    $userId,
+                    static::SET_ITEM_PREFIX . $setId,
+                    $completedContents / $totalContents,
+                    [ 'cascadeCreate' => true ]
+                );
+            $result = $this->client->send($request);
+        } catch(\Exception $e) {
+            report_async_error($e);
+        }
     }
 }
