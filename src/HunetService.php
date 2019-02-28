32b80f24cffebdc11ea03a93afb88f6e11003a90
hyunsoo <hyunsoo@publy.co>
2019-02-28 17:41:37 +0900

    add function

--- src/HunetService.php
+++ src/HunetService.php
@@ -6,33 +6,109 @@ use Publy\ServiceClient\Api\BaseApiService;
 
 class HunetService extends BaseApiService {
 
-    public function __construct() {
+    public function __construct($domain) {
         parent::__construct();
 
-        $this->domain = 'https://sample.com';
+        $this->domain = $domain;
         $this->apiUrl = "$this->domain/";
-
-        $this->timeout = 1;
-        $this->maxRetryCount = 10;
     }
 
-    public function subscriptionUserContentView()
+    public function pageviewChapter($hunetId, $contentId, $setId)
     {
-        
+        $retryCount = 3;
+        $properties = [
+            'hunet_id' => $hunetId,
+            'content_id' => $contentId,
+            'set_id' => $setId,
+            'com_id' => 'publy'
+        ];
+
+        $properties = json_encode($properties);
+
+        while ($retryCount > 0) {
+            try {
+                $this->post('hunet_test', ['event' => 'pageview_chapter', 'properties' => $properties]);
+                return respond_success();
+            } catch (\Exception $e) {
+                $retryCount--;
+                if ($retryCount == 0) {
+                    throw $e;
+                }
+            }
+        }
     }
 
-    public function completeUserContentProgress()
+    public function completeChapter($hunetId, $setId, $contentId)
     {
+        $retryCount = 3;
+        $properties = [
+            'hunet_id' => $hunetId,
+            'content_id' => $contentId,
+            'set_id' => $setId,
+            'com_id' => 'publy'
+        ];
 
+        $properties = json_encode($properties);
+
+        while ($retryCount > 0) {
+            try {
+                $this->post('hunet_test', ['event' => 'complete_chapter', 'properties' => $properties]);
+                return respond_success();
+            } catch (\Exception $e) {
+                $retryCount--;
+                if ($retryCount == 0) {
+                    throw $e;
+                }
+            }
+        }
     }
 
-    public function setReview()
+    public function rateSet($hunetId, $setId, $rating)
     {
+        $retryCount = 3;
+        $properties = [
+            'hunet_id' => $hunetId,
+            'set_id' => $setId,
+            'rating' => $rating,
+            'com_id' => 'publy'
+        ];
 
+        $properties = json_encode($properties);
+
+        while ($retryCount > 0) {
+            try {
+                $this->post('hunet_test', ['event' => 'rate_set', 'properties' => $properties]);
+                return respond_success();
+            } catch (\Exception $e) {
+                $retryCount--;
+                if ($retryCount == 0) {
+                    throw $e;
+                }
+            }
+        }
     }
 
-    public function setLike()
+    public function bookmark($hunetId, $setId)
     {
+        $retryCount = 3;
+        $properties = [
+            'hunet_id' => $hunetId,
+            'set_id' => $setId,
+            'com_id' => 'publy'
+        ];
 
+        $properties = json_encode($properties);
+
+        while ($retryCount > 0) {
+            try {
+                $this->post('hunet_test', ['event' => 'bookmark', 'properties' => $properties]);
+                return respond_success();
+            } catch (\Exception $e) {
+                $retryCount--;
+                if ($retryCount == 0) {
+                    throw $e;
+                }
+            }
+        }
     }
 }
