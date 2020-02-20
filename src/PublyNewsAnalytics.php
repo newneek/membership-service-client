b648dd5e33c400d5e540c7d13eb7a7bd9a387d92
Steve Baek <byeonghan@publy.co>
2020-02-20 13:28:58 +0900

    Add PublyNewsAnalyticsService

---
+++ src/PublyNewsAnalytics.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Publy\ServiceClient;
+
+use Publy\ServiceClient\Api\BaseApiService;
+
+class PublyNewsAnalyticsService extends BaseApiService
+{
+    protected $apiKey;
+
+    public function __construct($domain)
+    {
+        parent::__construct();
+
+        $this->domain = $domain;
+        $this->apiUrl = "$this->domain/";
+    }
+
+    public function sendEvent($eventBody)
+    {
+//        $eventBody =  [
+//            'event' => [
+//                'key' => 'eventKey',
+//                'properties' => ['foo' => 'bar'],
+//                'eventAt' => '2010-01-01 00:00:00'
+//            ],
+//            'user' => [
+//                'userId' => 2707
+//            ],
+//            'device' => [
+//                'deviceOS' => 'ios',
+//                'appVersion' => ''
+//            ]
+//        ];
+
+        return $this->post("/events", $eventBody);
+    }
+
+}
