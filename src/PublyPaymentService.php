1f30ee185188ad9d28e37c7bacbc10c110e9fdc2
jaeyong jung <jaeyong@publy.co>
2020-08-25 13:01:13 +0900

    PP-3709 add batch api (naverpay differences)

--- src/PublyPaymentService.php
+++ src/PublyPaymentService.php
@@ -3767,4 +3767,11 @@ class PublyPaymentService extends BaseApiService
             'batch' => $batch,
         ]);
     }
+
+    public function createNaverpayDifferenceBatch($changerId)
+    {
+        return $this->post("naverpay_difference/batch", [
+            'changer_id' => $changerId,
+        ]);
+    }
 }
