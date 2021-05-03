3b9076107dfa28c0838bb7713558d95ca6c8e91e
Hyunsoo Lee <hyunsoo@publy.co>
2021-05-03 22:12:48 +0900

    create skillup service

---
+++ src/PublySkillupService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Publy\ServiceClient;
+
+use Publy\ServiceClient\Api\BaseApiService;
+use Publy\ServiceClient\Api\ResponseException;
+
+class PublySkillupService extends BaseApiService {
+
+    public function __construct($domain) {
+        parent::__construct();
+
+        $this->domain = $domain;
+        $this->apiUrl = "$this->domain/";
+    }
+
+    public function createUserStatus($userId): array
+    {
+        return $this->post("user-status", [
+            'userId' => $userId
+        ]);
+    }
+}
